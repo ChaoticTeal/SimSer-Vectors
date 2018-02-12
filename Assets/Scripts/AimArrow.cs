a66c84a8b0218b5617a9886b44b72d2b2310bf92
PurpleNinjaLink <jawhittom@gmail.com>
2018-02-11 19:54:45 -0600

    Grid Functionality Added
    Aim arrow and target implemented. Target goes to the nearest vertex to the mouse cursor, arrow aims at the target.

---
+++ Assets/Scripts/AimArrow.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AimArrow : MonoBehaviour
+{
+    // Serialize fields - for assignment in the editor
+    // Target position
+    [SerializeField] Transform target;
+
+    // Private fields
+    // Angle between mouse position and default position
+    float angleDifference;
+    // Vertical vector
+    Vector2 defaultPosition = new Vector2(0f, 1f);
+	
+	// Update is called once per frame
+	void Update () 
+	{
+        // Rotate the arrow to face the mouse
+        Rotate();
+	}
+
+    // Rotates the arrow towards the mouse
+    void Rotate()
+    {
+        // Calculate angle between the target position and a vertical default vector
+        angleDifference = Vector2.SignedAngle(defaultPosition, target.position);
+        // Constrain angle between -90 and 90 degrees
+        angleDifference = Mathf.Min(angleDifference, 90f);
+        angleDifference = Mathf.Max(angleDifference, -90f);
+        // Rotate to the calculated angle
+        if(transform.rotation.z * Mathf.Rad2Deg <= 90f && transform.rotation.z * Mathf.Rad2Deg >= -90f)
+            transform.rotation = Quaternion.Euler(new Vector3(0f, 0f, angleDifference));
+    }
+}
---
+++ Assets/Scripts/AimTarget.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AimTarget : MonoBehaviour 
+{
+    // Private fields
+    // Mouse position in world coordinates
+    Vector3 mouseRelativePosition;
+	
+	// Update is called once per frame
+	void Update () 
+	{
+        Move();
+	}
+
+    // Move to the nearest grid point to the mouse cursor
+    private void Move()
+    {
+        // The following code adapted from https://answers.unity.com/questions/540888/converting-mouse-position-to-world-stationary-came.html
+        mouseRelativePosition = Input.mousePosition;
+        mouseRelativePosition.z = 10.0f; // Camera position
+        mouseRelativePosition = Camera.main.ScreenToWorldPoint(mouseRelativePosition);
+        // End adapted code
+
+        Vector2 newPosition = new Vector2(mouseRelativePosition.x, mouseRelativePosition.y);
+
+        // Round position to multiples of 2
+        // The grid is in Unity units of 2
+        if (newPosition.x % 2 != 0)
+            newPosition.x = RoundToTwo(newPosition.x);
+        if (newPosition.y % 2 != 0)
+            newPosition.y = RoundToTwo(newPosition.y);
+
+        transform.position = newPosition;
+    }
+
+    // Round the given value to the nearest multiple of 2
+    private float RoundToTwo(float f)
+    {
+        int nearest = (int)(f / 2);
+        float grid = f / 2;
+        if (grid >= 0 && (grid % 1f) * 10f >= 5f)
+            return (nearest + 1) * 2f;
+        else if (grid < 0 && (grid % 1f) * 10f <= -5f)
+            return (nearest - 1) * 2f;
+        else
+            return nearest * 2f;
+    }
+}
