4c790c7e94e7a2b143788be8ec125eb39ad3415f
PurpleNinjaLink <jawhittom@gmail.com>
2018-02-11 23:54:38 -0600

    Late Night Update 1
    Primary "gameplay" implemented - can aim and shoot and score points, data is displayed when shooting.

--- Assets/Scripts/AimArrow.cs
+++ Assets/Scripts/AimArrow.cs
@@ -1,25 +1,67 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class AimArrow : MonoBehaviour
 {
     // Serialize fields - for assignment in the editor
+    // Time to restrict movement and display data
+    [SerializeField] float delay;
+    // Slider for visual representation of vector
+    [SerializeField] Slider slider;
+    // Text for data display
+    [SerializeField] Text dataText;
     // Target position
     [SerializeField] Transform target;
 
     // Private fields
+    // Has the vector just been drawn?
+    bool cooldown = false;
+    // Should the vector be drawn?
+    bool shouldFire;
     // Angle between mouse position and default position
     float angleDifference;
     // Vertical vector
     Vector2 defaultPosition = new Vector2(0f, 1f);
+
+    // Properties
+    public bool Cooldown
+    {
+        get { return cooldown; }
+    }
 	
 	// Update is called once per frame
 	void Update () 
 	{
-        // Rotate the arrow to face the mouse
-        Rotate();
+        // Handle input
+        HandleInput();
+        if (!cooldown)
+        {
+            // Rotate the arrow to face the mouse
+            Rotate();
+            if(shouldFire)
+            // "Fire" the vector
+                Fire();
+        }
 	}
+
+    // Fire the vector
+    void Fire()
+    {
+        slider.value = target.position.magnitude;
+        dataText.text = "Vector: <" + target.position.x / 2 + "," + target.position.y / 2 + ">\nMagnitude: " + target.position.magnitude +
+            "\nDirection: " + (Vector2.SignedAngle(defaultPosition, target.position) + 90f) + "\u00B0";
+        StartCoroutine(VectorCooldown());
+    }
+
+    void HandleInput()
+    {
+        if (Input.GetMouseButtonDown(0))
+            shouldFire = true;
+        else
+            shouldFire = false;
+    }
 
     // Rotates the arrow towards the mouse
     void Rotate()
@@ -32,5 +74,14 @@ public class AimArrow : MonoBehaviour
         // Rotate to the calculated angle
         if(transform.rotation.z * Mathf.Rad2Deg <= 90f && transform.rotation.z * Mathf.Rad2Deg >= -90f)
             transform.rotation = Quaternion.Euler(new Vector3(0f, 0f, angleDifference));
+    }
+
+    IEnumerator VectorCooldown()
+    {
+        cooldown = true;
+        yield return new WaitForSeconds(delay);
+        cooldown = false;
+        dataText.text = "";
+        slider.value = 0;
     }
 }
---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,96 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour 
+{
+    // Serialize Fields for assignment in-editor
+    // Reference to aim arrow (to check cooldown)
+    [SerializeField] AimArrow arrow;
+    // Time a target is available
+    [SerializeField] float targetTime = 2f;
+    // Target
+    [SerializeField] GameObject target;
+    // Target score (required to continue)
+    [SerializeField] int requiredScore = 10;
+    // Score text
+    [SerializeField] Text scoreText;
+    // Player aim reticle
+    [SerializeField] Transform reticle;
+
+    // Private fields
+    // Did the player fire?
+    bool checkFire;
+    // Is there currently a target?
+    bool isTarget;
+    // Did the shot hit?
+    bool hit;
+    // Reference to active target
+    GameObject activeTarget;
+    // Score
+    int score = 0;
+
+	// Use this for initialization
+	void Start () 
+	{
+        UpdateText();
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+        HandleInput();
+        if (!isTarget)
+            StartCoroutine(ManageTarget());
+        if (activeTarget != null)
+            hit = activeTarget.transform.position == reticle.position && !arrow.Cooldown;
+        else
+            hit = false;
+        if (checkFire && hit)
+            HandleHit();
+	}
+
+    void HandleHit()
+    {
+        score++;
+        UpdateText();
+        if(activeTarget != null)
+            Destroy(activeTarget);
+    }
+
+    void HandleInput()
+    {
+        if (Input.GetMouseButtonDown(0))
+            checkFire = true;
+        else
+            checkFire = false;
+    }
+
+    void UpdateText()
+    {
+        scoreText.text = "Score: " + score;
+    }
+
+    // Generate a random point on the grid
+    Vector2 RandomPoint()
+    {
+        int x = Random.Range(-4, 5);
+        int y = Random.Range(0, 5);
+        if (x == 0 && y == 0)
+            y = Random.Range(1, 5);
+        return new Vector2(x * 2, y * 2);
+    }
+
+    IEnumerator ManageTarget()
+    {
+        activeTarget = Instantiate(target);
+        Vector2 newPosition = RandomPoint();
+        activeTarget.transform.position = new Vector3(RandomPoint().x, RandomPoint().y, 0f);
+        isTarget = true;
+        yield return new WaitForSeconds(targetTime);
+        if (activeTarget != null)
+            Destroy(activeTarget);
+        isTarget = false;
+    }
+}
